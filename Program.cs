aff1a4070667f625b28a7ffafb62f723055597af
marcelochirai <65429412+marcelochirai@users.noreply.github.com>
2020-11-23 21:54:33 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace POO_TarefaMatriz2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int m = int.Parse(line[0]); //Recebe o valor de m - linha
+            int n = int.Parse(line[0]); //Recebe o valor de n - coluna
+
+            int[,] mat = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            int x = int.Parse(Console.ReadLine());  //Recebe o valor de referência.
+
+            for (int i = 0; i < m; i++) //Exibe os valores ao redor da referência.
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] == x)
+                    {
+                        Console.WriteLine("Posição " + i + "," + j + ":");
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Esquerda: " + mat[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Acima: " + mat[i - 1, j]);
+                        }
+                        if (j < n - 1)
+                        {
+                            Console.WriteLine("Direita: " + mat[i, j + 1]);
+                        }
+                        if (i < m - 1)
+                        {
+                            Console.WriteLine("Abaixo: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
